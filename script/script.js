b2c7aadbf86a40bdaefc7f855a5334fbcfc9dabe
DanielJamesF <fredericksd17@gmail.com>
2022-05-14 09:25:02 +0200

    zeus

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function changeBg(){
+    var navbar = document.getElementById('navigation');
+    var scrollValue = window.scrollY;
+    if (scrollValue < 700){
+        navbar.classList.remove('bgColor')
+    }
+    else{
+        navbar.classList.add('bgColor')
+    }
+}
+window.addEventListener('scroll', changeBg);
+window.addEventListener('load', changeBg);
